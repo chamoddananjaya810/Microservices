2aa1974023a987cd6e10ee2522c12e4029bb0a7e
chamoddananjaya810 <chamoddhananjaya76@gmail.com>
2025-12-03 18:05:37 +0530

    Fix dependency error and start Eureka Server

---
+++ api-gateway/src/main/java/com.microservices.apigateway/ApiGatewayAplication.java
@@ -0,0 +1,12 @@
+package com.microservices.apigateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiGatewayAplication.class,args);
+    }
+}
